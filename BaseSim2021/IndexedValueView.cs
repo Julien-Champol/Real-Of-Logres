3d8608dba5e227bc4a0de332efaf1e250aa2f848
Julien Champol <julien.champol@etu.u-bordeaux.fr>
2021-05-08 19:57:05 +0200

    Indexed Value, Draw method

--- BaseSim2021/IndexedValueView.cs
+++ BaseSim2021/IndexedValueView.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace BaseSim2021
 {
@@ -17,23 +18,30 @@ namespace BaseSim2021
         private int y;
 
         /* Size of the rectangle. */
-        private int sizeRecatngle;
+        private int widthRectangle;
+        private int heightRectangle;
 
         /*The color of the rectangle */
         private Color color;
 
-        public IndexedValueView(IndexedValue index, int rectangleAbsciss, int rectangleOrdinate, int size, Color color)
+        public IndexedValueView(IndexedValue index, int rectangleAbsciss, int rectangleOrdinate, int width, int height, Color color)
         {
             this.IndexedValue = index;
             this.x = rectangleAbsciss;
             this.y = rectangleOrdinate;
-            this.sizeRecatngle = size;
+            this.widthRectangle = width;
+            this.heightRectangle = height;
             this.color = color;
         }
 
-        public void IndexedValueView_Draw()
+        public void IndexedValueView_Draw(PaintEventArgs e)
         {
-
+            Pen rectanglePen = new Pen(this.color, 3);
+            int absciss = this.x;
+            int ordinate = this.y;
+            int width = this.widthRectangle;
+            int height = this.heightRectangle;
+            e.Graphics.DrawRectangle(rectanglePen, absciss, ordinate, width, height);
         }
 
     }
